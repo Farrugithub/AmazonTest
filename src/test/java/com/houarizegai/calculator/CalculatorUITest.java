0409eef64fab0481b2e02ac8fa2018b364c1c67c
smfarhana <smfarhana2028@example.com>
2023-10-11 18:27:58 +0530

    added content

--- src/test/java/com/houarizegai/calculator/CalculatorUITest.java
+++ src/test/java/com/houarizegai/calculator/CalculatorUITest.java
@@ -3,7 +3,7 @@ package com.houarizegai.calculator;
 import com.houarizegai.calculator.ui.CalculatorUI;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvSources;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
